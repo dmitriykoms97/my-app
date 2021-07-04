46e309341b6cafdcb3ca315d942250d8b66ac718
dmitriykoms97 <workkoms97@gmail.com>
2021-07-04 20:12:54 +0300

    initial commit

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,7 +1,3 @@
-let rerenderEntireTree = () => {
-    console.log('State changed!');
-}
-
 export type postsType = {
     id: number
     message: string
@@ -35,7 +31,69 @@ export type RootStateType = {
     dialogsPage: dialogPageType
 }
 
-let state: RootStateType = {
+export type StoreType = {
+    _state: RootStateType
+    rerenderEntireTree: () => void
+    addPost: () => void
+    updateNewPostText: (newText: string) => void
+    subscribe: (observer: () => void) => void
+    getState: () => RootStateType
+}
+
+let store: StoreType = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: 1, message: 'Hi, how are you?', likeCount: 15},
+                {id: 2, message: 'Its my first post!', likeCount: 99},
+                {id: 2, message: 'Its my first post!', likeCount: 45},
+                {id: 2, message: 'Its my first post!', likeCount: 45},
+                {id: 2, message: 'Its my first post!', likeCount: 45}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogsData: [
+                {id: 1, name: 'Dmitriy', avatar: 'https://cdn.freelance.ru/images/att/1324133_900_600.png' },
+                {id: 2, name: 'Yulia', avatar: 'https://i.pinimg.com/736x/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg'},
+                {id: 3, name: 'Artemko', avatar: 'https://tiktok-wiki.ru/wp-content/uploads/2020/05/avatarki-dlya-tik-toka1.jpg'},
+                {id: 4, name: 'Eugen', avatar: 'https://tiktok-wiki.ru/wp-content/uploads/2020/05/avatarki-dlya-tik-toka1.jpg'}
+            ],
+            messageData: [
+                {id: 1, message: 'Hi', avatar: 'https://cdn.freelance.ru/images/att/1324133_900_600.png'},
+                {id: 2, message: 'How are you?', avatar: 'https://i.pinimg.com/736x/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg'},
+                {id: 3, message: 'Go playing in football...', avatar: 'https://cdn.freelance.ru/images/att/1324133_900_600.png'},
+                {id: 4, message: 'bla bla bla', avatar: 'https://i.pinimg.com/736x/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg'}
+            ]
+        }
+    },
+    rerenderEntireTree() {
+        console.log('State changed!');
+    },
+    addPost() {
+        debugger
+        let newPost: postsType = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 0
+        }
+        this._state.profilePage.postsData.unshift(newPost)
+        this._state.profilePage.newPostText = '';
+        this.rerenderEntireTree();
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText;
+        this.rerenderEntireTree();
+    },
+    subscribe(observer) {
+        this.rerenderEntireTree = observer;
+    },
+    getState() {
+        return this._state
+    }
+}
+
+/*let state: RootStateType = {
     profilePage: {
         postsData: [
             {id: 1, message: 'Hi, how are you?', likeCount: 15},
@@ -60,26 +118,5 @@ let state: RootStateType = {
             {id: 4, message: 'bla bla bla', avatar: 'https://i.pinimg.com/736x/64/a8/8f/64a88f80d6b5a43b58d14c20c7ef4b89.jpg'}
         ]
     }
-}
-
-export const addPost = () => {
-    let newPost: postsType = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 0
-    }
-    state.profilePage.postsData.unshift(newPost)
-    state.profilePage.newPostText = '';
-    rerenderEntireTree();
-}
-
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree();
-}
-
-export const subscribe = (observer: () => void) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+}*/
+export default store;
